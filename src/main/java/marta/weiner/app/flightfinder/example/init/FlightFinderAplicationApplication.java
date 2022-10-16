c14dcdf86227358e0db6efd5d4de1577dbd3c1dd
Marta.Weiner <martawei19@gmail.com>
2022-10-16 14:31:08 +0200

    start

---
+++ src/main/java/marta/weiner/app/flightfinder/example/init/FlightFinderAplicationApplication.java
@@ -0,0 +1,13 @@
+package marta.weiner.app.flightfinder.example.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFinderAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFinderAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marta/weiner/app/flightfinder/example/init/FlightFinderAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package marta.weiner.app.flightfinder.example.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFinderAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
