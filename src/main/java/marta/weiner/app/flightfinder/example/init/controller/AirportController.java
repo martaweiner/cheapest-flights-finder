d9c149478b77ab9dda5e1f1ca3bd4395f0962cad
Marta Weiner <74545877+martaweiner@users.noreply.github.com>
2022-11-08 16:30:23 +0100

    Add validation (#3)

--- src/main/java/marta/weiner/app/flightfinder/example/init/controller/AirportController.java
+++ src/main/java/marta/weiner/app/flightfinder/example/init/controller/AirportController.java
@@ -8,7 +8,6 @@ import marta.weiner.app.flightfinder.example.init.service.AirportService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,9 +19,8 @@ public class AirportController {
     private final AirportService service;
 
     @PostMapping
-    public AirportEntity add(@RequestBody AirportDto dto) {
+    public AirportEntity add(@RequestBody AirportDto dto) throws Exception {
         return service.save(mapper.map(dto));
-
     }
 
     @DeleteMapping(value = "/{id}")
@@ -30,20 +28,23 @@ public class AirportController {
         service.delete(id);
     }
 
+    @DeleteMapping("/deleteAll")
+    public void deleteAll() {
+        service.deleteAll();
+    }
+
     @GetMapping("/{id}")
-    public AirportDto getById(@PathVariable Long id){
+    public AirportDto getById(@PathVariable Long id) {
         return mapper.map(service.findById(id));
     }
 
     @GetMapping("/")
     public List<AirportDto> getAll() {
-        //List<AirportEntity> data = service.getAll();
-        //stream
         return service.getAll().stream().map(mapper::map).collect(Collectors.toList());
     }
 
     @PostMapping("/upload")
-    public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public void uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
         service.saveFromCsv(file);
     }
 }
--- src/main/java/marta/weiner/app/flightfinder/example/init/service/AirportService.java
+++ src/main/java/marta/weiner/app/flightfinder/example/init/service/AirportService.java
@@ -6,6 +6,8 @@ import marta.weiner.app.flightfinder.example.init.mapper.CSVMapper;
 import marta.weiner.app.flightfinder.example.init.repository.AirportRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import javax.validation.Constraint;
+import javax.validation.Payload;
 
 import javax.persistence.Id;
 import java.io.IOException;
@@ -18,7 +20,10 @@ public class AirportService {
     private final AirportRepository repository;
     private final CSVMapper csvMapper;
 
-    public AirportEntity save(AirportEntity entity) {
+
+    public AirportEntity save(AirportEntity entity) throws Exception {
+        validateShortname(entity);
+        validateName(entity);
         return repository.save(entity);
     }
 
@@ -34,8 +39,39 @@ public class AirportService {
         return repository.findById(id).orElse(null);
     }
 
-    public void saveFromCsv(MultipartFile file) throws IOException {
+    public void saveFromCsv(MultipartFile file) throws Exception {
             List<AirportEntity> data = csvMapper.csvToEntities(file.getInputStream());
-            repository.saveAll(data);
+        for (AirportEntity airport : data) {
+            save(airport);
+        }
+    }
+
+    public void deleteAll(){
+        repository.deleteAll();
+    }
+
+
+    private void validateShortname(AirportEntity entity) throws Exception {
+        if (entity.getShortname().length() != 3){
+            throw new Exception("Shortname must be 3 letters");
+        }
+        if (!entity.getShortname().chars().allMatch(Character::isLetter)){
+            throw new Exception("Shortname must be only letters");
+        }
+        if (repository.findByShortname(entity.getShortname())!=null){
+            throw new Exception("Shortname must be unique");
+        }
+    }
+
+    private void validateName(AirportEntity entity) throws Exception{
+        if (entity.getName().length() < 3){
+            throw new Exception("Name cannot be shorter than 3 letters");
+        }
+        if(!entity.getName().chars().allMatch((Character::isLetter))){
+            throw new Exception("Name must be only letters");
+        }
+        if (repository.findByName(entity.getName())!=null){
+            throw new Exception("Name must be unique");
+        }
     }
 }
